1d00c7ade1bec3f8f51c1df1c93c39b86371bac4
Costa <leancosta@deloitte.com>
2019-04-16 10:58:16 -0300

    Init commit

---
+++ public/js/app.js
@@ -0,0 +1,25 @@
+console.log('Client side JavaScript file loaded');
+
+const weatherForm = document.querySelector('form');
+const search = document.querySelector('input');
+const messageOne = document.getElementById('message-1');
+const messageTwo = document.getElementById('message-2');
+
+weatherForm.addEventListener('submit', e => {
+    e.preventDefault();
+
+    messageOne.textContent = 'Loading...';
+    messageTwo.textContent = ''
+
+    fetch(`http://localhost:3000/weather/?address=${search.value}`).then(response => {
+        response.json().then(data => {
+            if (data.error) {
+                messageOne.textContent = data.error;
+            } else {
+                const forecast = data.forecast;
+                messageOne.textContent = data.location;
+                messageTwo.textContent = `${forecast.summary} It is currently ${forecast.temperature} °C. There is ${forecast.precipProbability}% chance of rain.`;
+            }
+        });
+    });
+});
---
+++ src/utils/forecast.js
@@ -0,0 +1,21 @@
+const request = require('request');
+
+const forecast = (latitude, longitude, callback) => {
+    const url = `https://api.darksky.net/forecast/292ddd907859cb01d8739bfb7b8c3954/${latitude},${longitude}?units=si`;
+
+    request({url, json: true}, (error, {body}) => {
+        if (error) {
+            callback('Unable to connect to weather service', undefined);
+        } else if (body.error) {
+            callback('Unable to find location', undefined);
+        } else {
+            callback(undefined, {
+                summary: body.daily.data[0].summary,
+                temperature: body.currently.temperature,
+                precipProbability: body.currently.precipProbability * 100
+            });
+        }
+    });
+}
+
+module.exports = forecast;
