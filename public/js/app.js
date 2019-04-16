1812eeaac993f7e53305041d4fbf69f2fe021e9d
Costa <leancosta@deloitte.com>
2019-04-16 11:01:48 -0300

    Remove unecessary console.log call

--- public/js/app.js
+++ public/js/app.js
@@ -1,5 +1,3 @@
-console.log('Client side JavaScript file loaded');
-
 const weatherForm = document.querySelector('form');
 const search = document.querySelector('input');
 const messageOne = document.getElementById('message-1');
